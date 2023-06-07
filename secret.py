e84fc9c33bccfb482810467907f9ee832340d63e
Lightmaker777 <j.kochkorian@gmx.de>
2023-06-07 14:32:17 +0200

    Done Secret exercise

---
+++ secret.py
@@ -0,0 +1,27 @@
+secret = "there_is_no_secret_here"
+name = "Xxxxxx"
+year = "5000B.C."
+
+# Concatenate the secret with the name
+sec_plus_name = secret + name
+
+# Reverse the string
+sec_plus_name_rev = sec_plus_name[::-1]
+
+# Concatenate the reversed string with the year 
+
+result = sec_plus_name_rev + year
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
